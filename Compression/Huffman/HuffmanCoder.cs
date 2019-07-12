3028058fad9e17f3182408feb1be89656ab4fad0
Adam Szerszeń <adam_szerszen@interia.pl>
2019-07-12 14:52:58 +0200

    Added Huffman project with basic Huffman coder to develop;

---
+++ Compression/Huffman/HuffmanCoder.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using Infrastructure.interfaces;
+
+namespace Huffman
+{
+    public class HuffmanCoder : IEncoder, IDecoder
+    {
+        private Dictionary<char, string> _dictionary;
+
+        public string Decode(string cipher)
+        {
+            if (_dictionary != null)
+            {
+            }
+        }
+
+        public string Encode(string plaintext)
+        {
+        }
+
+        public void LoadDictionary(Dictionary<char, string> dictionary)
+        {
+            _dictionary = dictionary;
+        }
+
+        public Dictionary<char, string> GetDictionary()
+        {
+            return _dictionary;
+        }
+    }
+}
